37f099d966e1a136eec843be86df0967a95d478c
AmruthaKumarK <amruthakumark19@gmail.com>
2023-11-09 09:17:29 +0530

    updates

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-<<<<<<< HEAD
-// Select all elements with the "i" tag and store them in a NodeList called "stars"
-const stars = document.querySelectorAll(".stars i");
 
-// Loop through the "stars" NodeList
-stars.forEach((star, index1) => {
-  // Add an event listener that runs a function when the "click" event is triggered
-  star.addEventListener("click", () => {
-    // Loop through the "stars" NodeList Again
-    stars.forEach((star, index2) => {
-      // Add the "active" class to the clicked star and any stars with a lower index
-      // and remove the "active" class from any stars with a higher index
-      index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
-    });
-  });
-});
-=======
 // Get all star elements
 const stars = document.querySelectorAll(".star");
 
@@ -64,5 +48,3 @@ function resetStars() {
         star.style.transform = "scale(1)";
     });
 }
-
->>>>>>> 22e8e27eadc9a962c4410cc859af6c565b51f627
